2894e04428202eca853133e292b62da1d6c8b500
чяго <randomRandint.@gmail.com>
2024-11-01 12:53:59 +0300

    рограмма

---
+++ test/test.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from random import randint
+screen=Tk()
+screen.geometry(f"{randint(1,1000)}x{randint(1,1000)}")
+n=randint(-1000000,1000000)
+def sumM(a,b):
+    return a+b
+def minus(a,b):
+    return a-b
+def umnozzzzzzzzzzzzzzzhenie(a,b):
+    return a*b
+print(sumM(2,3))
+print(minus(5,2))
+print(umnozzzzzzzzzzzzzzzhenie(2,3))
+label1=Label(text="")
+label1.pack()
+label2=Label(text="")
+label2.pack()
+Button(text="                                                                                                                ").pack()
+screen.mainloop()
